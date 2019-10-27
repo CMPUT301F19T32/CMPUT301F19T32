8fe4b60109a49eccb6afadf8a84c197915260903
ChenXinyue11 <46543999+ChenXinyue11@users.noreply.github.com>
2019-10-27 17:04:45 -0600

    Mood Base class

---
+++ code/MentaLand/app/src/main/java/com/example/mentaland/Mood.java
@@ -0,0 +1,67 @@
+package com.example.mentaland;
+
+public class Mood {
+    private String emotionstr;
+    private String emotionState;
+    private String reason;
+    private String time;
+    private  String date;
+    private String socialState;
+    private String username;
+
+    public String getEmotionstr() {
+        return emotionstr;
+    }
+
+    public String getEmotionState() {
+        return emotionState;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getSocialState() {
+        return socialState;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmotionstr(String emotionstr) {
+        this.emotionstr = emotionstr;
+    }
+
+    public void setEmotionState(String emotionState) {
+        this.emotionState = emotionState;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setSocialState(String socialState) {
+        this.socialState = socialState;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
