a8e6a4262e10ceca7c30efcdefcbba05cc9b5095
ChenXinyue11 <46543999+ChenXinyue11@users.noreply.github.com>
2019-10-27 18:56:55 -0600

    FriendPage processing

--- code/MentaLand__FriendPage/app/src/main/java/com/example/mentaland__friendpage/MainActivity.java
+++ code/MentaLand__FriendPage/app/src/main/java/com/example/mentaland__friendpage/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.mentaland__friendpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView moodFriendList;
+    ArrayAdapter<Mood> moodFrArrayAdapter;
+    ArrayList<Mood> moodFrArrayList;
+    private int index;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moodFriendList = findViewById(R.id.friendLIstVeiw);
+        moodFrArrayList = new ArrayList<>();
+        moodFrArrayAdapter = new CustomeFriendList(this, moodFrArrayList);
+        moodFriendList.setAdapter(moodFrArrayAdapter);
+        Button requestButton = findViewById(R.id.request);
+        Button map = findViewById(R.id.map_fr);
+        Button addFriend = findViewById(R.id.addFriend);
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index = moodFrArrayList.size() + 1;
+                new addFriendFrag().show(getSupportFragmentManager(),"addFriend");
+
+            }
+        });
+
+
     }
 }
---
+++ code/MentaLand__FriendPage/app/src/main/java/com/example/mentaland__friendpage/addFriendFrag.java
@@ -0,0 +1,14 @@
+package com.example.mentaland__friendpage;
+
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+public class addFriendFrag extends DialogFragment {
+    private String userName;
+    private EditText userToSent;
+    private EditText massage;
+
+
+
+}
