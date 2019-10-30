9127333386b4af77d23022e5242f3962e42e67dc
masterzh <masterzh@masterzhdeMBP.hitronhub.home>
2019-10-30 15:21:25 -0600

    request processing by xinyue11

--- code/MentaLand__FriendPage/app/src/main/java/com/example/mentaland__friendpage/RequestActivity.java
+++ code/MentaLand__FriendPage/app/src/main/java/com/example/mentaland__friendpage/RequestActivity.java
@@ -1,6 +1,8 @@
 package com.example.mentaland__friendpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +16,7 @@ public class RequestActivity extends AppCompatActivity {
     ListView requestVeiw;
     ArrayList<Request> requestArrayList;
     ArrayAdapter<Request> requestArrayAdapter;
+    Request request;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +24,12 @@ public class RequestActivity extends AppCompatActivity {
         setContentView(R.layout.request_layout);
         username = getIntent().getStringExtra("user");
 //onclick view list both
-        
+        requestVeiw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 
 }
