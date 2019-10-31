7718f40d8f20e319495625e33eeb765eb5f3cf36
Turing98 <44661836+Turing98@users.noreply.github.com>
2019-10-31 17:56:29 -0600

    create

--- code/MentaLand/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ code/MentaLand/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080024;
-        public static final int end = 0x7f080044;
-        public static final int gone = 0x7f080055;
-        public static final int invisible = 0x7f080062;
-        public static final int left = 0x7f080067;
-        public static final int packed = 0x7f08007f;
-        public static final int parent = 0x7f080081;
-        public static final int percent = 0x7f080084;
-        public static final int right = 0x7f08008a;
-        public static final int spread = 0x7f0800ae;
-        public static final int spread_inside = 0x7f0800af;
-        public static final int start = 0x7f0800b4;
-        public static final int top = 0x7f0800cc;
-        public static final int wrap = 0x7f0800db;
+        public static final int bottom = 0x7f080023;
+        public static final int end = 0x7f080043;
+        public static final int gone = 0x7f080053;
+        public static final int invisible = 0x7f080060;
+        public static final int left = 0x7f080065;
+        public static final int packed = 0x7f08007d;
+        public static final int parent = 0x7f08007f;
+        public static final int percent = 0x7f080082;
+        public static final int right = 0x7f080088;
+        public static final int spread = 0x7f0800ab;
+        public static final int spread_inside = 0x7f0800ac;
+        public static final int start = 0x7f0800b1;
+        public static final int top = 0x7f0800c9;
+        public static final int wrap = 0x7f0800d8;
     }
     public static final class styleable {
         private styleable() {}
