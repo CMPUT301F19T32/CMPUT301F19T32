1b91df449a4e4c5a2a3ddab84a08dcf6065dad32
Puquan <46555082+Puquan@users.noreply.github.com>
2019-11-08 07:19:29 -0700

    fix bug that cannot attch location if GPS is initially enabled or APP is firstly installed

--- code/LogIn/app/src/main/java/com/example/myapplication/EditMoodActivity.java
+++ code/LogIn/app/src/main/java/com/example/myapplication/EditMoodActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -248,8 +249,33 @@ public class EditMoodActivity extends AppCompatActivity {
 
             return new LatLng(0,0);
         }
+        final Location myLocation = lm.getLastKnownLocation(GPS_PROVIDER);
+        lm.requestLocationUpdates(GPS_PROVIDER, 1000, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                myLocation.setLatitude(location.getLatitude());
+                myLocation.setLongitude(location.getLongitude());
+            }
 
-        Location myLocation = lm.getLastKnownLocation(GPS_PROVIDER);
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+
+
+
+
         if (myLocation == null) {
             return new LatLng(0,0);
         }
