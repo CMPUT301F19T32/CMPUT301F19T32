f639f5f600c21943735caf5ccca74556d2a4f157
Yiyang Wang <32249739+yiyang-evan@users.noreply.github.com>
2019-11-08 02:08:11 -0700

    GeoLocation Test and Mood test

---
+++ code/LogIn/app/src/test/java/com/example/myapplication/GeoLocationTest.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GeoLocationTest {
+
+    private Geolocation createGeolocation(){
+
+        Geolocation geolocation = new Geolocation(53.484310, -113.506133);
+
+        return geolocation;
+    }
+
+    @Test
+
+    void testGeolocation(){
+        Geolocation geolocation = createGeolocation();
+        assertEquals(geolocation.getLatitude(),53.484310);
+        geolocation.setLatitude(53.484566);
+        assertEquals(geolocation.getLatitude(),53.484566);
+
+        assertEquals(geolocation.getLongitude(),-113.506133);
+        geolocation.setLongitude(-113.506244);
+        assertEquals(geolocation.getLongitude(),-113.506244);
+
+    }
+
+    //@Ignore("null case cannot be tested")
+    //@Test
+    //void test(){
+    //Geolocation geolocation = null;
+    //assertEquals(geolocation.getLongitude(),null);
+
+    //}
+}
