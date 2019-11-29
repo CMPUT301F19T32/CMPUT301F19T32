60a37c1befa72f0f72fa293d07e8071464a47a27
TURING-LIU <zl8@ualberta.ca>
2019-11-29 12:14:24 -0700

    test

--- code/Part 4/app/src/androidTest/java/com/example/myapplication/AddMoodActivityTest.java
+++ code/Part 4/app/src/androidTest/java/com/example/myapplication/AddMoodActivityTest.java
@@ -1,46 +1,30 @@
 package com.example.myapplication;
 
 import android.app.Activity;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.Spinner;
 
-import androidx.annotation.NonNull;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.robotium.solo.Solo;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static androidx.test.espresso.action.ViewActions.click;
 
-import org.junit.Rule;
-
-/**
- * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
- used
- */
 public class AddMoodActivityTest {
     private Solo solo;
     @Rule
-    public ActivityTestRule<AddMoodActivity> rule =
-            new ActivityTestRule<>(AddMoodActivity.class, true, true);
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
     /**
      * Runs before all tests and creates solo instance.
      * @throws Exception
@@ -58,20 +42,12 @@ public class AddMoodActivityTest {
         Activity activity = rule.getActivity();
     }
 
-
-    /**
-     * Add a mood to the EditText view and check the username using assertTrue
-     */
     @Test
-    public void checkAddMood(){
-        /*
+    public void checkEditMoodList(){
         solo.enterText((EditText) solo.getView(R.id.username_field), "user1");
         solo.enterText((EditText) solo.getView(R.id.password_field), "123");
         solo.clickOnButton("SIGN IN");
-        solo.clickOnButton("ADD MOOD");
-        */
-        solo.assertCurrentActivity("Wrong Activity", AddMoodActivity.class);
-
+        solo.clickOnView(solo.getView((R.id.button)));
         View view1 = solo.getView("imageView");
 
         solo.clickOnView(view1);
@@ -87,13 +63,8 @@ public class AddMoodActivityTest {
 
         solo.clickOnButton("submit");
 
-
-
+        solo.assertCurrentActivity("Wrong Activity", AddMoodActivity.class);
 
 
     }
-
-
-
-
 }
